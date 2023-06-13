7201b7dbc39ccf9e248a558b226371446978f296
chris mcmains <mcmainsc84@gmail.com>
2023-06-13 17:16:53 -0400

    completed methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your Favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"{name} has a favorite color which is {color}.  He is fond of {animal}. {name} and {animal} like to listen to {band} on the radio.  Maybe {name} should paint the {animal} {color} and name it {band}.");
+
+
+
         }
+        public static int Sum(int a, int b)
+    {
+            int sum = a + b;
+            return sum;
+            
+}       
+     
+        public static int multiply(int num1, int num2)
+        { return num1 * num2; }
+
     }
 }
