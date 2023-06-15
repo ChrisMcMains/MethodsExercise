c24d57aa62ae1cd17ed8c5b34d6b3cb90c74d1aa
chris mcmains <mcmainsc84@gmail.com>
2023-06-15 17:05:42 -0400

    MethodsComplete2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,32 +1,67 @@
-﻿namespace MethodsExercise
+﻿using Microsoft.VisualBasic;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine("What is your Favorite color?");
-            var color = Console.ReadLine();
-            Console.WriteLine("What is your favorite animal?");
-            var animal = Console.ReadLine();
-            Console.WriteLine("What is your favorite band?");
-            var band = Console.ReadLine();
 
-            Console.WriteLine($"{name} has a favorite color which is {color}.  He is fond of {animal}. {name} and {animal} like to listen to {band} on the radio.  Maybe {name} should paint the {animal} {color} and name it {band}.");
+            MyMessage();
 
-
-
-        }
-        public static int Sum(int a, int b)
-    {
-            int sum = a + b;
-            return sum;
             
-}       
-     
-        public static int multiply(int num1, int num2)
-        { return num1 * num2; }
+            
+            
+        }
+        
 
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int multiply(int num1, int num2)
+        { 
+            return num1 * num2; 
+        
+        }
+        public static void MyMessage()
+        {
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your Favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"{name} has a favorite color which is {color}.  He is fond of {animal}." +
+                $"{name} and {animal} like to listen to {band} on the radio.  Maybe {name} should paint " +
+                $"the {animal} {color} and name it {band}.");
+
+            Console.WriteLine("Give me a number to add.");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the last input.");
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = Sum(a, b);
+            Console.WriteLine($"The sum is:{sum}");
+
+
+            Console.WriteLine("Give me a number to multiply.");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply to the last input.");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int product = multiply(num1, num2);
+            Console.WriteLine($"The answer is:{product}");
+            Console.ReadLine();
+        }
     }
 }
+
